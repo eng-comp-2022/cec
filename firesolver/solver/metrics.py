56eae8ae95bb3714d45e5c27751a72a23db11923
Justen Di Ruscio <justend29@gmail.com>
2022-03-12 13:26:21 -0400

    calc statistically significant data

--- firesolver/solver/metrics.py
+++ firesolver/solver/metrics.py
@@ -1,5 +1,7 @@
 import dataloader
 import numpy as np
+from statsmodels.stats.weightstats import ztest as ztest
+import math
 
 
 def grid_metrics():
@@ -7,5 +9,24 @@ def grid_metrics():
     return np.multiply(rain_data, 1.0)
 
 
-def significant():
-    pass
+def significant_areas(data):
+    alpha = 0.05
+    _, p_vals = ztest(data, value=np.mean(data))
+    return p_vals >= alpha
+
+
+def Duff_Moisture_Content(ave_rainfall):
+
+    DCM = np.empty(shape=(ave_rainfall.shape()))
+    i = 0
+    for x in ave_rainfall:
+        j = 0
+        for y in x:
+            Pe = 0.92 * ave_rainfall[i, j]
+            M = 1000
+            DMC[i, j] = 244.72 - 43.43 * math.log(())
+
+
+if __name__ == "__main__":
+    # print(np.mean(grid_metrics()))
+    print(significant_areas(grid_metrics()))
