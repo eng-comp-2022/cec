50397b3d8cb56d124ac1d9137f83a18226d894e5
Greg Kean <gregorykean@outlook.com>
2022-03-12 13:48:46 -0400

    updated zone data again

--- data/zoneBuilder.py
+++ data/zoneBuilder.py
@@ -23,8 +23,8 @@ for i in range(width):
         for j in range(height):
             json_data["features"].append({"type": "Feature", "properties": {"AREA": 1}, "geometry": {"type": "Polygon", "coordinates": [[[lat, lon], [lat, lon-eps], [lat+eps, lon-eps], [lat+eps, lon]]]}, "id": str(id)})
             id += 1
-            lon -= eps
-        lat += eps2
+            lon -= eps2
+        lat += eps
         lon = top_left_lon
         
 with open("zoneData.json", "w") as f:
