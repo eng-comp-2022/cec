790586ca5aa30a4954d68fe0437a2ebfa55e9860
Greg Kean <gregorykean@outlook.com>
2022-03-12 14:54:30 -0400

    hbhbbubububuub

--- data/zoneBuilder.py
+++ data/zoneBuilder.py
@@ -7,8 +7,8 @@
 
 import json
 
-top_left_lon = -67.779559
-top_left_lat = 47.846172
+top_left_lat = -67.66776684614666
+top_left_lon = 47.74668297874867
 eps = 0.007992
 width = 246
 height = 304
@@ -16,15 +16,15 @@ height = 304
 json_data = {"type": "FeatureCollection", "features": []}
 id = 0
 
-lat = top_left_lon
-lon = top_left_lat
+lat = top_left_lat
+lon = top_left_lon
 for i in range(width):
         for j in range(height):
             json_data["features"].append({"type": "Feature", "properties": {"AREA": 1}, "geometry": {"type": "Polygon", "coordinates": [[[lat, lon], [lat, lon-eps], [lat+eps, lon-eps], [lat+eps, lon]]]}, "id": str(id)})
             id += 1
             lon -= eps
         lat += eps
-        lon = top_left_lat
+        lon = top_left_lon
         
 with open("/Users/gregkean/Desktop/EngComp/cec/data/zoneData.json", "w") as f:
         json.dump(json_data, f)
