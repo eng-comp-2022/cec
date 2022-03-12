c9c90de41071bcbf3a1e9cb2a69e0d62e3519bfd
Greg Kean <gregorykean@outlook.com>
2022-03-12 14:32:40 -0400

    undid work

--- data/zoneBuilder.py
+++ data/zoneBuilder.py
@@ -7,10 +7,9 @@
 
 import json
 
-top_left_lon = -67.66776684614666
-top_left_lat = 47.74668297874867
-eps = 0.01125514978108399
-eps2 = 0.0079
+top_left_lat = -67.66776684614666
+top_left_lon = 47.74668297874867
+eps = 0.007992
 width = 246
 height = 304
 
@@ -21,11 +20,11 @@ lat = top_left_lat
 lon = top_left_lon
 for i in range(width):
         for j in range(height):
-            json_data["features"].append({"type": "Feature", "properties": {"AREA": 1}, "geometry": {"type": "Polygon", "coordinates": [[[lon, lat], [lon, lat-eps], [lon+eps, lat-eps], [lon+eps, lat]]]}, "id": str(id)})
+            json_data["features"].append({"type": "Feature", "properties": {"AREA": 1}, "geometry": {"type": "Polygon", "coordinates": [[[lat, lon], [lat, lon-eps], [lat+eps, lon-eps], [lat+eps, lon]]]}, "id": str(id)})
             id += 1
-            lat -= eps
-        lon += eps
-        lat = top_left_lat
+            lon -= eps
+        lat += eps
+        lon = top_left_lon
         
-with open("zoneData.json", "w") as f:
+with open("/Users/gregkean/Desktop/EngComp/cec/data/zoneData.json", "w") as f:
         json.dump(json_data, f)
