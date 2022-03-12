11198aeb4408ade8b1485408e41f39363319fb4e
Greg Kean <gregorykean@outlook.com>
2022-03-12 13:47:06 -0400

    Remade zone data

--- data/zoneBuilder.py
+++ data/zoneBuilder.py
@@ -10,6 +10,7 @@ import json
 top_left_lat = -67.66776684614666
 top_left_lon = 47.74668297874867
 eps = 0.01125514978108399
+eps2 = 0.0079
 width = 246
 height = 304
 
@@ -23,7 +24,7 @@ for i in range(width):
             json_data["features"].append({"type": "Feature", "properties": {"AREA": 1}, "geometry": {"type": "Polygon", "coordinates": [[[lat, lon], [lat, lon-eps], [lat+eps, lon-eps], [lat+eps, lon]]]}, "id": str(id)})
             id += 1
             lon -= eps
-        lat += eps
+        lat += eps2
         lon = top_left_lon
         
 with open("zoneData.json", "w") as f:
--- firesolver/maps/chloroplethMap.py
+++ firesolver/maps/chloroplethMap.py
@@ -1,24 +1,28 @@
 from urllib.request import urlopen
 import json
 import pandas as pd
+import plotly.express as px
 
+#Generate the heat map of risk for each tile
 def generateRiskMap(saveMap, filePath):
-    with urlopen('https://raw.githubusercontent.com/eng-comp-2022/cec/main/data/zoneData.json') as response:
-        zones = json.load(response)
-
     
-    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+    #Open and load the generated json file containing the longitudes and latitudes for each zone
+    with urlopen('https://raw.githubusercontent.com/eng-comp-2022/cec/main/data/zoneData.json') as response:
+        zoneData = json.load(response)
+
+    #Open and load the csv containing the generated risk values for each zones
+    riskData = pd.read_csv("https://raw.githubusercontent.com/eng-comp-2022/cec/main/data/metric_output.csv",
                     dtype={"fips": str})
 
-    import plotly.express as px
-
-    fig = px.choropleth_mapbox(df, geojson=zones, locations='fips', color='unemp',
+    #Create the choropleth map using the data loaded above
+    fig = px.choropleth_mapbox(riskData, geojson=zoneData, locations='fips', color='unemp',
                             color_continuous_scale="Viridis",
                             range_color=(0, 1),
                             mapbox_style="carto-positron",
                             zoom=6.5, center = {"lon":-66.170557, "lat":46.652598},
                             opacity=0.5,
-                            labels={'unemp':'Fire Probability'}
+                            labels={'unemp':'Fire Probability'},
+                            title="Fire Probability Heat Map",
                             )
-   
+    fig.update_traces(marker_line_width=0)
     fig.show()
