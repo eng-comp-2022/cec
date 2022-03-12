8cfdee9baa6aa3f39037c26e9d8925971c679334
pwalsh2 <paul.N.walsh@outlook.ca>
2022-03-12 19:09:50 -0400

    hahaha

--- firesolver/solver/metrics.py
+++ firesolver/solver/metrics.py
@@ -3,11 +3,18 @@ import numpy as np
 from statsmodels.stats.weightstats import ztest as ztest
 import math
 import sys
-
+import fire_resource_plan
 # np.set_printoptions(threshold=sys.maxsize)
 
 
 def grid_metrics():
+    """
+    grid metric. This calc the grid metric that is used the calculate the 
+    probability of the fire in  a given location. This is based on a number of facters that 
+    were provided .... rainfall, temp, firework etc. Along with some other metrics DMC and DC indexes.
+
+    :return: M x N grid of probabilities
+    """ 
     rain_data = dataloader.get_data("average_rainfall")
     foliage_data = dataloader.get_norm_data("average_foliage_density")
     temp = dataloader.get_norm_data("average_predic_temp")
@@ -31,6 +38,12 @@ def grid_metrics():
 
 
 def significant_areas(data, z_threshold=1):
+    """
+    significant areas: function to determine the sig.
+    :param data: describe about parameter p1
+    :param z_threshold: describe about parameter p2
+    :return: the statistically significant areas based on the z_threshold
+    """ 
     sample_mean = np.mean(data)
     sample_std = np.std(data)
 
@@ -42,6 +55,17 @@ def significant_areas(data, z_threshold=1):
 
 
 def duff_moisture_content(ave_rainfall):
+    """
+    duff_moisture_content. Used in metric funct
+
+    What is a Duff Moisture content?
+    The Duff Moisture Code (DMC) is a numeric rating of the average moisture 
+    content of loosely compacted organic layers of moderate depth.
+    This code gives an indication of fuel consumption in moderate duff layers and medium-size woody material.
+    ref: https://cwfis.cfs.nrcan.gc.ca/background/summary/fwi
+    :param ave_rainfall: the average rainfall provided
+    :return: the duff moisture index for the specific location
+    """ 
     def moisture_content_element(element):
         if element <= 1.5:
             return 0
@@ -59,6 +83,18 @@ def duff_moisture_content(ave_rainfall):
 
 
 def drought_code(ave_rainfall):
+    """
+    drought code used in metric funct
+
+    what is a drought code??
+    The Drought Code (DC) is a numeric rating of the average moisture content of deep, 
+    compact organic layers. This code is a useful indicator of seasonal drought effects on 
+    forest fuels and the amount of smoldering in deep duff layers and large logs.
+    ref: https://cwfis.cfs.nrcan.gc.ca/background/summary/fwi
+
+    :param ave_rainfall: the average rainfall provided by the csv file 
+    :return: the drought code index for the specific area 
+    """ 
     def drought_code_element(element):
         Pe = 0.92 * element
         Q = 3.937 * Pe
